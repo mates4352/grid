eac6120deb1f540b0caa0488c1a0d5fff7a29e02
Sergey <cabyrkih@mail.ru>
2021-02-10 18:03:32 +0300

    grid

--- js/app.js
+++ js/app.js
@@ -5,3 +5,32 @@ burger.addEventListener('click', ()=> {
 	burger.classList.toggle('active')
 	menu.classList.toggle('active')
 })
+
+const title = document.querySelector('.header__title')
+let text = 'Ali Sayed’s <span>Web Designing</span> Project'
+let text2 = 'Ali Sayed’s Web Designing Project'
+let count = 0
+let text1 = ''
+let timer;
+
+function a() {
+	timer = setTimeout(function () {
+		if (count < text2.length){
+		text1 = text2[count]
+		title.innerHTML += text1 
+		count++
+		a()
+		}
+		else {
+			clearTimeout(timer)
+			title.innerHTML = text
+		}
+
+	}, getRandom(getRandom(500 * 2.5)))
+}
+
+function getRandom(max) {
+	return Math.floor(Math.random() * Math.floor(max))
+}
+
+a()
