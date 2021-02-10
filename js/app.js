9b6f161777dc40a8fb90b577aee12dfbcae1c920
Sergey <cabyrkih@mail.ru>
2021-02-10 15:17:26 +0300

    menu

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 const burger = document.querySelector('.burger')
+const menu = document.querySelector('.menu')
 
 burger.addEventListener('click', ()=> {
 	burger.classList.toggle('active')
+	menu.classList.toggle('active')
 })
